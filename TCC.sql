a365a19618a2accc6d5800bb61738cf7095a12a9
RafLeal <165904115+RafLeal@users.noreply.github.com>
2024-05-18 18:06:40 -0300

    Update TCC.sql

--- TCC.sql
+++ TCC.sql
@@ -1,13 +1,11 @@
-SELECT
-    t.Titulo AS Titulo_TCC,
-    p.Nome AS Nome_Professor_Orientador,
-    a.RA,
-    a.Nome AS Nome_Aluno
-FROM
-    TCC t
-JOIN
-    Professor p ON t.Professor_ID = p.Professor_ID
-JOIN
-    TCC_Aluno ta ON t.TCC_ID = ta.TCC_ID
-JOIN
-    Alunos a ON ta.RA = a.RA
+SELECT 
+    O.Orientacao_ID,
+    O.Titulo,
+    A1.Nome AS Aluno1,
+    A2.Nome AS Aluno2,
+    P.Nome AS Professor_Orientador
+FROM 
+    Orientacao_TCC O
+    JOIN Alunos A1 ON O.RA = A1.RA
+    JOIN Alunos A2 ON O.RA2 = A2.RA
+    JOIN Professor P ON O.Professor_ID = P.Professor_ID;
